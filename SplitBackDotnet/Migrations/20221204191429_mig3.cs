bd58ee87af58250cdb5a5e9161499cf813d66282
ChristosKehayas <christos.kechagia@gmail.com>
2022-12-04 19:38:49 +0000

    commit message

---
+++ SplitBackDotnet/Migrations/20221204191429_mig3.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SplitBackDotnet.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PendingTransaction");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PendingTransaction",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    GroupId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Amount = table.Column<decimal>(type: "TEXT", nullable: false),
+                    CurrentGroupId = table.Column<int>(type: "INTEGER", nullable: false),
+                    IsoCode = table.Column<string>(type: "TEXT", maxLength: 3, nullable: false),
+                    ReceiverId = table.Column<int>(type: "INTEGER", nullable: false),
+                    SenderId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PendingTransaction", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PendingTransaction_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "GroupId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PendingTransaction_GroupId",
+                table: "PendingTransaction",
+                column: "GroupId");
+        }
+    }
+}
