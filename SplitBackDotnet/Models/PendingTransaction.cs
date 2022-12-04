6ead92421cd944a85cefc9eabe4bc4de4c4004ed
thanoskat <thanoskat@users.noreply.github.com>
2022-12-04 22:52:38 +0200

    format doc

--- SplitBackDotnet/Models/PendingTransaction.cs
+++ SplitBackDotnet/Models/PendingTransaction.cs
@@ -1,11 +1,13 @@
-using System.ComponentModel.DataAnnotations;
 namespace SplitBackDotnet.Models;
 
-public class PendingTransaction
-{
+public class PendingTransaction {
+
   public int SenderId { get; set; }
+
   public int ReceiverId { get; set; }
+
   public decimal Amount { get; set; }
+
   public string IsoCode { get; set; } = null!;
 }
 
