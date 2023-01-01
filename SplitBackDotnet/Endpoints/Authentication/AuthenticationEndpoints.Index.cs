170fc0103d519c84e9c8e7a036a7af492c86b6e2
thanoskat <thanoskatiras@gmail.com>
2023-01-01 05:19:51 +0200

    draft

--- SplitBackDotnet/Endpoints/Authentication/AuthenticationEndpoints.Index.cs
+++ SplitBackDotnet/Endpoints/Authentication/AuthenticationEndpoints.Index.cs
@@ -1,13 +1,13 @@
 ﻿namespace SplitBackDotnet.Endpoints.Authentication;
 
-public static partial class AuthenticationEndpoints
-{
-  public static void MapAuthenticationEndpoints(this IEndpointRouteBuilder app) {
+public static partial class AuthenticationEndpoints {
   
+  public static void MapAuthenticationEndpoints(this IEndpointRouteBuilder app) {
+
     var authGroup = app.MapGroup("/auth")
       .WithTags("Authentication")
       .AllowAnonymous();
-    
+
     authGroup.MapPost("/verify-token", VerifyToken);
     authGroup.MapPost("/sign-in", SignIn);
     authGroup.MapPost("/request-sign-up", RequestSignUp);
--- SplitBackDotnet/Endpoints/Authentication/AuthenticationEndpoints.RefreshToken.cs
+++ SplitBackDotnet/Endpoints/Authentication/AuthenticationEndpoints.RefreshToken.cs
@@ -8,32 +8,32 @@ namespace SplitBackDotnet.Endpoints.Authentication;
 
 public static partial class AuthenticationEndpoints {
 
-    private static async Task<IResult> RefreshToken(HttpRequest request, IConfiguration config, IRepo repo) {
-        var refreshToken = request.Cookies["refreshToken"];
-        if(refreshToken == null) return Results.BadRequest();
+  private static async Task<IResult> RefreshToken(HttpRequest request, IConfiguration config, IRepo repo) {
+    var refreshToken = request.Cookies["refreshToken"];
+    if(refreshToken == null) return Results.BadRequest();
 
-        var sessionFound = await repo.GetSessionByRefreshToken(refreshToken);
-        if(sessionFound == null) return Results.BadRequest();
+    var sessionFound = await repo.GetSessionByRefreshToken(refreshToken);
+    if(sessionFound == null) return Results.BadRequest();
 
-        var secureKey = Encoding.UTF8.GetBytes(config["Jwt:Key"]);
-        var securityKey = new SymmetricSecurityKey(secureKey);
-        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512);
-        var jwtTokenHandler = new JwtSecurityTokenHandler();
+    var secureKey = Encoding.UTF8.GetBytes(config["Jwt:Key"]);
+    var securityKey = new SymmetricSecurityKey(secureKey);
+    var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512);
+    var jwtTokenHandler = new JwtSecurityTokenHandler();
 
-        var tokenDescriptor = new SecurityTokenDescriptor {
-            Subject = new ClaimsIdentity(new[] {
+    var tokenDescriptor = new SecurityTokenDescriptor {
+      Subject = new ClaimsIdentity(new[] {
       new Claim("userId", sessionFound.UserId)
     }),
-            Expires = DateTime.Now.AddMinutes(10),
-            Audience = config["Jwt:Audience"],
-            Issuer = config["Jwt:Issuer"],
-            SigningCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512)
-        };
+      Expires = DateTime.Now.AddMinutes(10),
+      Audience = config["Jwt:Audience"],
+      Issuer = config["Jwt:Issuer"],
+      SigningCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512)
+    };
 
-        var token = jwtTokenHandler.CreateToken(tokenDescriptor);
+    var token = jwtTokenHandler.CreateToken(tokenDescriptor);
 
-        return Results.Ok(new {
-            accessToken = jwtTokenHandler.WriteToken(token)
-        });
-    }
+    return Results.Ok(new {
+      accessToken = jwtTokenHandler.WriteToken(token)
+    });
+  }
 }
